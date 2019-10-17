03ddd0bfdca858e4ce72ee9af4f4d2f67e286c57
BrunoBoligon <brunoboligon@hotmail.com>
2019-10-17 20:05:43 -0300

    Refatoração dos metodos

--- app/services/calculadora.rb
+++ app/services/calculadora.rb
@@ -1,25 +1,47 @@
 class Calculadora
+
+   attr_reader :valor1, :valor2
+
+   def initialize(valor1, valor2)
+      raise StandardError, 'A primeira variavel não é um numero' if !valor1.is_a? Float
+      raise StandardError, 'A primeira variavel não é um numero' if !valor2.is_a? Float
+
+
+      @valor1 = valor1
+      @valor2 = valor2
+   end
+
+   def self.somar(valor1, valor2)
+      new(valor1, valor2).somar
+   end
+
+   def self.diminuir(valor1, valor2)
+      new(valor1, valor2).diminuir
+   end
+
+   def self.dividir(valor1, valor2)
+      new(valor1, valor2).dividir
+   end
+
+   def self.multiplicar(valor1, valor2)
+      new(valor1, valor2).multiplicar
+   end
+
    def somar(valor1, valor2)
-   raise StandardError if !valor1.is.a? Float
- 
-      valor1 + valor2
+      @valor1 + @valor2
    end
 
    def diminuir(valor1, valor2)
-   raise StandardError if !valor1.is.a? Float
-   
-      valor1 - valor2
+      @valor1 - @valor2
    end
 
    def dividir(valor1, valor2)
-   raise StandardError if !valor1.is.a? Float
-      
-      valor1 / valor2
+      raise ZeroDivisionError, 'Não é permitido dividir por 0 seu animal' if @valor2 == 0
+
+      @valor1 / @valor2
    end
 
    def multiplicar(valor1, valor2)
-   raise StandardError if !valor1.is.a? Float
-         
-      valor1 * valor2
+      @valor1 * @valor2
    end
 end
--- spec/services/calculadora_spec.rb
+++ spec/services/calculadora_spec.rb
@@ -1,13 +1,95 @@
 RSpec.describe Calculadora, type: :services do
-    describe "ao somar" do
-      it "o número 5 e 3, o resultado deve ser 8" do
-       calculadora = Calculadora.new
-        expect (calculadora.somar(5,3)).to eq(8)
-      end
 
-      it "passano A e 3, o resultado deve ser uma exceção"
-      calculadora = Calculadora.new
-        expect (calculadora.somar('A',3)).to eq(8)
-      end
+  describe "ao instanciar" do
+    it "passando a letra A e 3, o resultado deve ser uma exceção" do
+      expect do 
+        calculadora = Calculadora.new('A',3.0)
+      end.to raise_error(StandardError, 'A primeira variavel não é um numero')
+    end
+
+    it "passando 16 e uma letra C, o resultado deve ser uma exceção" do
+      expect do 
+        calculadora = Calculadora.new(16.0,'C')
+      end.to raise_error(StandardError, 'A segunda variavel não é um numero')
+    end
+
+    it "passando as letras A e C, o resultado deve ser uma exceção" do
+      expect do 
+        calculadora = Calculadora.new('A','C')
+      end.to raise_error(StandardError, 'A primeira variavel não é um numero')
     end
   end
+
+# -------------------------------------------------------------------------------------------------
+
+  describe "ao somar" do
+
+      it "o número 5 e 3, o resultado deve ser 8" do
+        calculadora = Calculadora.new(5.0, 3.0)
+        expect(calculadora.somar).to eq(8)
+      end
+
+      it "passando os numeros -15 e 8, o resultado deve ser -7" do
+        calculadora = Calculadora.new(-15.0, 8.0)
+        expect(calculadora.somar).to eq(-7)
+      end
+  end
+
+# -------------------------------------------------------------------------------------------------
+
+  describe "ao subtrair" do
+
+    it "o número 156 e 134, o resultado deve ser 22" do
+      calculadora = Calculadora.new(156.0, 134.0)
+      expect(calculadora.diminuir).to eq(22)
+    end
+
+    it "passando os numeros -15 e 5, o resultado deve ser -20" do
+      calculadora = Calculadora.new(-15.0, 5.0)
+      expect(calculadora.somar).to eq(-20)
+    end
+  end
+
+# -------------------------------------------------------------------------------------------------
+
+  describe "ao dividir" do
+
+    it "o número 12 e 6, o resultado deve ser 2" do
+      calculadora = Calculadora.new(12.0, 6.0)
+      expect(calculadora.dividir).to eq(2)
+    end
+
+    it "passando os numeros -15 e 5, o resultado deve ser -20" do
+      calculadora = Calculadora.new(-15.0, 5.0)
+      expect(calculadora.dividir).to eq(-20)
+    end
+
+    it "dividindo 18 por 0, o resultado deve ser um erro de divisão por 0" do
+      expect do
+        calculadora = Calculadora.new(18.0, 0)
+        calculadora.dividir
+      end.ro raise_error(ZeroDivisionError, 'Não é permitido dividir por 0 seu animal')
+    end
+  end
+
+# -------------------------------------------------------------------------------------------
+
+describe "ao multiplicar" do
+
+    it "o número -2 e 4, o resultado deve ser 8" do
+      calculadora = Calculadora.new(-2.0, 4.0)
+      expect(calculadora.multiplicar).to eq(8)
+    end
+
+    it "passando os numeros 3 e -5, o resultado deve ser -15" do
+      calculadora = Calculadora.new(3.0, -5.0)
+      expect(calculadora.multiplicar).to eq(-15)
+    end
+
+    it "passando os numeros -5 e -5, o resultado deve ser 25" do
+      calculadora = Calculadora.new(3.0, 5.0)
+      expect(calculadora.multiplicar).to eq(25)
+    end
+    
+  end
+end
